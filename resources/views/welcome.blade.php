e2570b302facc2ff1369ef1e4ba8661e31445fc5
Jhoss Web <jhossweb@gmail.com>
2024-07-08 23:24:51 -0400

    mostrando variables de entorno

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -161,7 +161,7 @@
                             </div>
                         </div>
                     </main>
-
+                                            {{ env('APP_URL') }}
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
